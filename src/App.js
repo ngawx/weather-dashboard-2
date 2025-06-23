d1721773fd59042c41eb7aea8fffc38ada7ba42e
ngawx <roonerp@gmail.com>
2025-06-22 22:13:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ function App() {
       <iframe
         title="Facebook Feed"
         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FNorthGaWxCommand&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        width="100%"
+        width="125%"
         height="600"
         style={{ border: "none", overflow: "hidden" }}
         scrolling="no"
